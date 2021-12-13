5d8e0d0619e68b80277056709b967c77a5db47aa
Jaime Cedillo <74879881+jaimecedillo@users.noreply.github.com>
2021-12-12 17:33:14 -0700

    added number to each cell

--- src/Board/Board.jsx
+++ src/Board/Board.jsx
@@ -1,6 +1,22 @@
 import React, { useState } from 'react';
 
 import './Board.css';
+class LinkedListNode {
+    constructor(value) {
+        this.value = value;
+        this.value = null;
+    }
+}
+
+class SinglyLinkedalist {
+    constructor(value) {
+        const node = new LinkedListNode(value);
+        this.value = value;
+        this.value = null;
+    }
+}
+
+
 const BOARD_SIZE = 10;
 
 const Board = () => {
@@ -23,5 +39,18 @@ const Board = () => {
         </div>
     );
 };
+
+const createBoard = BOARD_SIZE => {
+    let counter = 1;
+    const board = [];
+    for (let row = 0; row < BOARD_SIZE; row++) {
+        const currentRow = [];
+        for (let col = 0; col < BOARD_SIZE; col++) {
+            currentRow.push(counter++);
+        }
+        board.push(currentRow);
+    }
+    return board;
+}
 
 export default Board;

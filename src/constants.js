ac0593f4dddef5378810c9d5de1a51d90ac5583d
Jaime Cedillo <74879881+jaimecedillo@users.noreply.github.com>
2021-12-14 20:21:21 -0700

    added constants.js for functions

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+const CANVAS_SIZE = [800, 800];
+const SNAKE_START = [
+    [0, 1],
+    [0, 2]
+];
+
+const APPLE_START = [8, 3];
+const SCALE = 40;
+const SPEED = 100;
+const DIRECTIONS = {
+    38: [0, -1],
+    40: [0, 1],
+    37: [-1, 0],
+    39: [1, 0],
+
+};
+
+export {
+
+
+}
